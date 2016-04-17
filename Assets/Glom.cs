4f448f42f38be6987eb06e4eab9b07d4dc940ade
Wes Hardee <weshardee@gmail.com>
2016-04-17 14:18:46 -0500

    make glom springy

--- Assets/Glom.cs
+++ Assets/Glom.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class Glom : MonoBehaviour {
 	// editor components
-	public FixedJoint2D glomJoint;
+	public SpringJoint2D glomJoint;
 	public LayerMask layerMask;
 	
 	// state flags
 	public bool CanGlom = true;
 	public bool IsGlommed { get; private set; }
 	
+	// active glom info
+	private Vector2 _GlomPoint;
+	
+	private const float _Radius = 0.5f;
+	
 	void Start () {
-		glomJoint = gameObject.AddComponent<FixedJoint2D>();
+		// create and configure joint
+		glomJoint = gameObject.AddComponent<SpringJoint2D>();
+		glomJoint.autoConfigureConnectedAnchor = false;
+		glomJoint.autoConfigureDistance = false;
+		glomJoint.distance = 0;
+		glomJoint.enableCollision = true;
 		glomJoint.enabled = false;
+		glomJoint.frequency = 10;
 	}
 	
 	void Update () {
@@ -23,11 +34,14 @@ public class Glom : MonoBehaviour {
 		// }
 		
 		// Debug.Log(IsGlommed);
+		if (IsGlommed) {
+			Vector2 anchorInWorldSpace = glomJoint.anchor + (Vector2)transform.position;
+			Debug.DrawLine(anchorInWorldSpace, glomJoint.connectedAnchor, Color.green);
+		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll){
 		if (!IsGlommed && CanGlom) {
-			Debug.Log("tada");
 			GlomTo(coll);
 		}
 	}
@@ -35,5 +49,10 @@ public class Glom : MonoBehaviour {
 	void GlomTo(Collision2D coll) {
 		IsGlommed = true;	
 		glomJoint.enabled = true;
+		_GlomPoint = coll.contacts[0].point;
+		
+		// set the point of contact as the connected anchor point of the glomJoint
+		Vector2 point = (Vector2)coll.contacts[0].point;
+		glomJoint.connectedAnchor = point;
 	}
 }
