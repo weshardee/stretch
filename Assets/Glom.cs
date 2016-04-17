4eee522e20c6f32374c3851e900ccf4f4f8bd915
Wes Hardee <weshardee@gmail.com>
2016-04-17 12:47:38 -0500

    add glom behavior

---
+++ Assets/Glom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glom : MonoBehaviour {
+	// editor components
+	public SpringJoint2D glomSpring;
+	public LayerMask layerMask;
+	
+	// state flags
+	public bool CanGlom = true;
+	public bool IsGlommed { get; private set; }
+	
+	void Start () {
+	}
+	
+	void Update () {
+		// if (!IsGlommed && CanGlom) {
+		// 	// check for collision
+		// 	bool shouldGlom = body.IsTouchingLayers(layerMask);
+		// 	GlomTo()
+		// }
+		
+		// Debug.Log(IsGlommed);
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll){
+		if (!IsGlommed && CanGlom) {
+			Debug.Log("tada");
+			GlomTo(coll);
+		}
+	}
+
+	void GlomTo(Collision2D coll) {
+		IsGlommed = true;	
+	}
+}
