ba2ab33317fe940a3be49b1c5e002ca16e6cfed7
Wes Hardee <weshardee@gmail.com>
2016-04-17 16:24:41 -0500

    disconnect scale from stretch component

--- Assets/ScaleWithStretch.cs
+++ Assets/ScaleWithStretch.cs
@@ -7,6 +7,7 @@ public class ScaleWithStretch : MonoBehaviour {
 	public Transform end2;
 	
 	private const float _ScaleAtMaxStretch = 0.7f;
+	private const float _MaxStretch = 5f;
 	
 	// Use this for initialization
 	void Start () {
@@ -16,7 +17,7 @@ public class ScaleWithStretch : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float distance = (end1.position - end2.position).sqrMagnitude;	
-		float howStretched = distance / Stretch.MaxStretch;
+		float howStretched = distance / _MaxStretch;
 		Vector2 scale = Vector2.one * Mathf.Lerp(1, _ScaleAtMaxStretch, howStretched);		
 		end1.localScale = scale;
 		end2.localScale = scale;
