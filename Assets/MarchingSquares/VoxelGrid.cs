17ba93761881c84ae93b9de2952d682185e4787f
Wes Hardee <weshardee@gmail.com>
2016-04-23 20:35:12 -0500

    add grid

--- Assets/MarchingSquares/VoxelGrid.cs
+++ Assets/MarchingSquares/VoxelGrid.cs
@@ -2,16 +2,38 @@
 
 public class VoxelGrid : MonoBehaviour {
     [SerializeField]
-    private Transform voxelPrefab;
-    public int resolution;
+    private GameObject voxelPrefab;
+    [SerializeField]
+    private int resolution;
 
     bool[] voxels;
+    private float voxelSize;
+    private const float VoxelGutter = 0.02f;
 
     void Awake () {
         voxels = new bool[resolution * resolution];
+        voxelSize = 1f / resolution;
+
+        // initialize voxels
+        for (int i = 0, y = 0; y < resolution; y++) {
+            for (int x = 0; x < resolution; x++, i++) {
+                CreateVoxel(i, x, y);
+            }
+        }
+    }
+
+    void Start() {
+
     }
 
     void Update () {
 
+    }
+
+    void CreateVoxel(int i, int x, int y) {
+        GameObject v = Instantiate(voxelPrefab) as GameObject;
+        v.transform.parent = transform;
+        v.transform.localPosition = new Vector2(x * voxelSize, y * voxelSize);
+        v.transform.localScale = Vector2.one * (voxelSize - VoxelGutter);
     }
 }
