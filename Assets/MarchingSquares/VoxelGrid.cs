272646175f53b10694afcf0d4aee1b6734a6af59
Wes Hardee <weshardee@gmail.com>
2016-04-23 21:37:09 -0500

    add voxel map

--- Assets/MarchingSquares/VoxelGrid.cs
+++ Assets/MarchingSquares/VoxelGrid.cs
@@ -3,17 +3,21 @@
 public class VoxelGrid : MonoBehaviour {
     [SerializeField]
     private GameObject voxelPrefab;
-    [SerializeField]
     private int resolution;
+    private float size;
 
     bool[] voxels;
     private float voxelSize;
     private const float VoxelGutter = 0.02f;
 
-    void Awake () {
+    public void Initialize (int resolution, float size) {
+        Debug.Log(resolution);
+        this.resolution = resolution;
+        this.size = size;
         voxels = new bool[resolution * resolution];
-        voxelSize = 1f / resolution;
+        voxelSize = size / resolution;
 
+        Debug.Log(voxels.Length);
         // initialize voxels
         for (int i = 0, y = 0; y < resolution; y++) {
             for (int x = 0; x < resolution; x++, i++) {
@@ -33,7 +37,7 @@ public class VoxelGrid : MonoBehaviour {
     void CreateVoxel(int i, int x, int y) {
         GameObject v = Instantiate(voxelPrefab) as GameObject;
         v.transform.parent = transform;
-        v.transform.localPosition = new Vector2(x * voxelSize, y * voxelSize);
+        v.transform.localPosition = new Vector2((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
         v.transform.localScale = Vector2.one * (voxelSize - VoxelGutter);
     }
 }
---
+++ Assets/MarchingSquares/VoxelMap.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class VoxelMap : MonoBehaviour {
+    [SerializeField]
+    private VoxelGrid voxelGridPrefab;
+
+    [SerializeField]
+    private int size;
+    [SerializeField]
+    private int chunkResolution;
+    [SerializeField]
+    private int voxelResolution;
+
+    bool[] chunks;
+
+    private float halfSize;
+    private float voxelSize;
+    private float chunkSize;
+
+    void Awake () {
+        halfSize = size / 2;
+        chunkSize = size / chunkResolution;
+        voxelSize = chunkSize / voxelResolution;
+
+        chunks = new bool[size * size];
+
+        // initialize voxels
+        for (int i = 0, y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++, i++) {
+                CreateChunk(i, x, y);
+            }
+        }
+    }
+
+    void Start() {
+
+    }
+
+    void Update () {
+
+    }
+
+    void CreateChunk(int i, int x, int y) {
+        VoxelGrid chunk = Instantiate(voxelGridPrefab) as VoxelGrid;
+        chunk.transform.parent = transform;
+        chunk.transform.localPosition = new Vector2(x * chunkSize - halfSize, y * chunkSize - halfSize);
+
+        chunk.Initialize(chunkResolution, chunkSize);
+    }
+}
