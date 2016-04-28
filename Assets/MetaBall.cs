a1d6a891bc5176d337c2cf1b80437651f32a583d
Wes Hardee <weshardee@gmail.com>
2016-04-27 21:13:46 -0500

    add testing scene for metaballs

---
+++ Assets/MetaBall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MetaBall : MonoBehaviour {
+    public float power = 1;
+
+	void OnDrawGizmos() {
+		Gizmos.DrawSphere(transform.position, power);
+	}
+
+    public float CalcInfluence(Vector2 globalPoint) {
+        float distance = (globalPoint - (Vector2)transform.position).sqrMagnitude;
+        float influence = 1 - distance * power;
+        return influence;
+    }
+}
