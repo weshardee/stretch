4d9770a0f2d4131b0a5bef41116666f1e0a787c7
Wes Hardee <weshardee@gmail.com>
2016-04-28 21:22:09 -0500

    update influence calc

--- Assets/MetaBalls/MetaBall.cs
+++ Assets/MetaBalls/MetaBall.cs
@@ -11,10 +11,21 @@ public class MetaBall : MonoBehaviour {
     {
         // TODO performance bottleneck? Maybe cache position on update?
         Vector2 deltaVector = point - (Vector2)transform.localPosition;
-        float distance = deltaVector.sqrMagnitude;
-        float influence = 1 - distance / power;
+        float r = deltaVector.sqrMagnitude;
+
+        if (r > power)
+        {
+            return 0;
+        }
+
+        // adjust radius by a factor
+        r *= power;
+
+        float influence = r * r * r * (r * (r * 6 - 15) + 10); // http://www.geisswerks.com/ryan/BLOBS/blobs.html
+
         if (influence > 1) influence = 1;
         if (influence < 0) influence = 0;
-        return influence;
+
+        return 1 - influence;
     }
 }
