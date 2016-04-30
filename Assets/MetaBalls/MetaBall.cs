5c586f9d30f2ad35a3d74fa5c5af7535aded1c65
Wes Hardee <weshardee@gmail.com>
2016-04-30 00:49:21 -0500

    simplify metaball calc

--- Assets/MetaBalls/MetaBall.cs
+++ Assets/MetaBalls/MetaBall.cs
@@ -13,19 +13,11 @@ public class MetaBall : MonoBehaviour {
         Vector2 deltaVector = point - (Vector2)transform.localPosition;
         float r = deltaVector.sqrMagnitude;
 
-        //if (r > power)
-        //{
-        //    return 0;
-        //}
-
-        // adjust radius by a factor
-        r /= power;
-
-        float influence = r * r * r * (r * (r * 6 - 15) + 10); // http://www.geisswerks.com/ryan/BLOBS/blobs.html
+        var influence = (power*power)/(r*r);
 
         if (influence > 1) influence = 1;
         if (influence < 0) influence = 0;
 
-        return 1 - influence;
+        return influence;
     }
 }
